a5e5fbaed95c2dd3a4cd557f0efa65c631b9eafa
James Hyun <blackj0221@pm.me>
2020-03-24 16:59:39 +0900

    Python 코드 수정

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ if __name__ == "__main__":
     print('안녕 Python!')
     
     num = 0
-    li = []
+    li = [x + 1 for x in range(5)]
+    
+    for i in range(len(li)):
+        print(li[i], end=' ')
+    
+    print()
 
